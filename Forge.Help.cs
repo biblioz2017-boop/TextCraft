f7867a43582c579d7ef4623756cc90b0d2647333
biblioz2017-boop <biblioz2017@gmail.com>
2026-08-31 22:07:59 +0400

    Stage chunked embedded manual loader

--- Forge.Help.cs
+++ Forge.Help.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Reflection;
+using System.Text;
 using System.Windows.Forms;
 using Microsoft.Office.Tools.Ribbon;
 using Word = Microsoft.Office.Interop.Word;
@@ -9,7 +10,16 @@ namespace TextForge
 {
     partial class Forge
     {
-        private const string HelpResourceName = "TextForge.NeZnaikaManualRU.docx";
+        private static readonly string[] HelpResourceParts =
+        {
+            "TextForge.NeZnaikaManualRU.Part01",
+            "TextForge.NeZnaikaManualRU.Part02",
+            "TextForge.NeZnaikaManualRU.Part03",
+            "TextForge.NeZnaikaManualRU.Part04",
+            "TextForge.NeZnaikaManualRU.Part05",
+            "TextForge.NeZnaikaManualRU.Part06"
+        };
+
         private const string HelpFileName = "NeZnaika-1.0.41-Manual-RU.docx";
 
         private void HelpButton_Click(object sender, RibbonControlEventArgs e)
@@ -57,21 +67,21 @@ namespace TextForge
             }
 
             Assembly assembly = typeof(Forge).Assembly;
-            using (Stream source = assembly.GetManifestResourceStream(HelpResourceName))
+            StringBuilder encoded = new StringBuilder(90000);
+            foreach (string resourceName in HelpResourceParts)
             {
-                if (source == null)
-                    throw new InvalidOperationException("Встроенное руководство НеZнайка не найдено в ресурсах надстройки.");
-
-                using (FileStream target = new FileStream(
-                    helpPath,
-                    FileMode.Create,
-                    FileAccess.Write,
-                    FileShare.Read
-                ))
+                using (Stream part = assembly.GetManifestResourceStream(resourceName))
                 {
-                    source.CopyTo(target);
+                    if (part == null)
+                        throw new InvalidOperationException("Встроенное руководство НеZнайка повреждено: отсутствует ресурс " + resourceName + ".");
+
+                    using (StreamReader reader = new StreamReader(part, Encoding.ASCII, false))
+                        encoded.Append(reader.ReadToEnd());
                 }
             }
+
+            byte[] manualBytes = Convert.FromBase64String(encoded.ToString());
+            File.WriteAllBytes(helpPath, manualBytes);
 
             application.Documents.Open(
                 FileName: helpPath,
